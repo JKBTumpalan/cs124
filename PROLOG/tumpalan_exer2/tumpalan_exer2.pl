b4495892bd623dbfd7bcf12b8968d94dc19efcec
JKBTumpalan <jbtumpalan@up.edu.ph>
2020-09-23 17:28:42 +0800

    PROLOG exercise_revised

--- PROLOG/tumpalan_exer2/tumpalan_exer2.pl
+++ PROLOG/tumpalan_exer2/tumpalan_exer2.pl
@@ -1,28 +1,14 @@
-course(cs124).
-course(cs132).
-course(cs170).
-course(cs141).
-course(cs125).
-takehome(cs124_exers).
-takehome(cs170_exers).
-submit(annabeth, cs132_exers).
-submit(annabeth, cs141_exers).
-submit(rachel,cs141_exers).
-submit(calypso, cs170_exers).
-pass(rachel, cs125_exams).
-pass(rachel, cs132_exams).
-pass(calypso, cs124_exams).
-pass(calypso, cs125_exams).
-pass(annabeth, cs170_exams).
-pass(annabeth, cs141_exams).
-finish(X, cs124) :- pass(X, cs124_exams);
-                    submit(X,cs124_exers), takehome(cs124_exers).
-finish(X, cs125) :- pass(X, cs125_exams);
-                    submit(X,cs125_exers), takehome(cs125_exers).
-finish(X, cs141) :- pass(X, cs141_exams);
-                    submit(X,cs141_exers), takehome(cs141_exers).
-finish(X, cs132) :- pass(X, cs132_exams);
-                    submit(X,cs132_exers), takehome(cs132_exers).
-finish(X, cs170) :- pass(X, cs170_exams);
-                    submit(X,cs170_exers), takehome(cs170_exers).
-                
+takehome(cs124).
+takehome(cs170).
+submit(annabeth, cs132).
+submit(annabeth, cs141).
+submit(rachel,cs141).
+submit(calypso, cs170).
+pass(rachel, cs125).
+pass(rachel, cs132).
+pass(calypso, cs124).
+pass(calypso, cs125).
+pass(annabeth, cs170).
+pass(annabeth, cs141).
+finish(X, Z) :- pass(X, Z);
+                submit(X,Z), takehome(Z).
